9666bcce422d3a516ed0eef6d9c3144fd450a516
Yash Mittal <yashmittal2009@gmail.com>
2016-06-15 15:03:21 +0200

    Demo toolbar and pretty layouts

---
+++ In-class/03-ToolbarDemo/app/src/main/java/hu/ait/android/toolbardemo/MainActivity.java
@@ -0,0 +1,44 @@
+package hu.ait.android.toolbardemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionStart:
+                Toast.makeText(MainActivity.this, "Start", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.actionHelp:
+                Toast.makeText(MainActivity.this, "Help", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.actionAbout:
+                Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
