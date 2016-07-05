c64cc76f4f815d8967774887f846eae158cb3d28
Yash Mittal <yashmittal2009@gmail.com>
2016-07-05 10:06:58 +0200

    I build a MediaPlayer yay

---
+++ In-class/20-MusicPlayer/app/src/main/java/hu/ait/android/musicplayer/MainActivity.java
@@ -0,0 +1,33 @@
+package hu.ait.android.musicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this,
+                        R.raw.music);
+                mp.setOnPreparedListener(MainActivity.this);
+            }
+        });
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
+}
